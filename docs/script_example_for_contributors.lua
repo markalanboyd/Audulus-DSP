35dce5e29a0d171211acf7bd8ec1287965071098
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-30 09:22:46 +0200

    Added info for contributors

---
+++ docs/script_example_for_contributors.lua
@@ -0,0 +1,55 @@
+--[[
+
+[NAME OF SCRIPT]
+by [YOUR NAME]
+v [VERSION NUMBER]
+[MONTH_NAME DATE, YEAR]
+[YOUR WEBSITE URL, EMAIL, ETC.]
+
+[Concisely describe what the script does here. Each line should be a
+maximum of 79 characters long. If you don't have a ruler set in your
+text editor, just upload it and I'll fix it.
+
+Make sure to break up your description into multiple lines.
+
+This library is meant to be perused by absolute beginners, so don't be
+afraid to over-explain things. If you're not sure what to write, just
+look at some of the other scripts and copy their format.
+
+If your code is particularly complicated, you can and should add
+comments within the code block below. However, if it's just a few lines,
+you can just describe it here and leave the code clean.
+
+Please prefer descriptive variable names over comments. For example,
+instead of writing: 
+
+    -- This variable is the increment
+    local incr = hzIn[i] / sampleRate
+
+You should write:
+
+    local increment = hzIn[i] / sampleRate
+
+If you're REALLY lazy and don't want to write a description, you can 
+just upload it and I'll add one ;) If I have any questions about how it
+works, I'll ask you.
+
+Thank you!]
+
+--]]
+
+-- Inputs: [INPUTS separated by spaces]
+-- Outputs: [OUTPUTS separated by spaces]
+
+GLOBAL_CONSTANT = 0
+globalVariable = 0
+
+function exampleFunction()
+    local localVariable = 0
+end
+
+function process(frames)
+    for i = 1, frames do
+        -- [YOUR CODE HERE]
+    end
+end
