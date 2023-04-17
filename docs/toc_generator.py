4ccb4c1396eaf7a52ffd7b1ba72e2b35890465c0
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-04-17 16:58:10 +0200

    Trying to add automatic TOC

---
+++ docs/toc_generator.py
@@ -0,0 +1,28 @@
+import os
+
+def generate_toc(root, dir_name):
+    toc = []
+    for dirpath, dirnames, _ in os.walk(os.path.join(root, dir_name)):
+        level = dirpath.replace(root, '').count(os.sep)
+        indent = '  ' * (level - 1)
+
+        if level > 0:
+            dirname = os.path.basename(dirpath)
+            link = os.path.join(dirpath).replace(root, '.')
+            toc.append(f"{indent}- [`{dirname}`]({link})\n")
+
+    return ''.join(toc)
+
+def main():
+    root_dir = './code'
+    output_file = 'table_of_contents.md'
+
+    toc = generate_toc(root_dir, '')
+
+    with open(output_file, 'w') as f:
+        f.write(toc)
+
+    print(f"Table of contents generated and saved to {output_file}")
+
+if __name__ == "__main__":
+    main()
