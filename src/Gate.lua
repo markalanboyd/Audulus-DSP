6837ef0cf57aebc95e2ba7111c1ef4066de472a2
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-24 22:53:58 +0100

    Create Gate.lua
    - Add Gate class for assisting in creating one sample pulses that don't fire again until the gate goes low

---
+++ src/Gate.lua
@@ -0,0 +1,26 @@
+Gate = {}
+G = Gate
+Gate.__index = Gate
+
+function Gate.new()
+    local self = setmetatable({}, Gate)
+    self.state = 0
+    self.latch = false
+    return self
+end
+
+function Gate:to_pulse(gate)
+    local gate_high = gate > 0
+    if not gate_high then
+        self.latch = false
+        return self.state
+    end
+
+    if not self.latch then
+        self.state = 1
+        self.latch = true
+    else
+        self.state = 0
+    end
+    return self.state
+end
