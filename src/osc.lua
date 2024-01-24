a4ce86744914d6393995f53cd17ea26aaf09d4da
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-24 22:55:11 +0100

    Update osc.lua
    - Add update method and shape methods - you now update each sample and then have access to several waveshapes from one central phasor.

--- src/osc.lua
+++ src/osc.lua
@@ -4,13 +4,38 @@ Osc.__index = Osc
 
 function Osc.new()
     local self = setmetatable({}, Osc)
-    self.phasor = Phasor.new()
+    self.__gate = Gate.new()
+    self.__phasor = Phasor.new()
+    self.__phase = 0
+    self.hz = 1
+    self.phase_offset = 0
+    self.sync = 0
+    self.shape = 0
     return self
 end
 
-function Osc:sine(hz, phase_offset)
-    local po = phase_offset or 0
-    return math.sin(
-        self.phasor:audio(hz) + po
-    )
+function Osc:update()
+    self.__phase = self.__phasor:audio(
+        self.hz,
+        self.__gate:to_pulse(self.sync)
+    ) + self.phase_offset * Math.pi2
+end
+
+function Osc:sine()
+    return math.sin(self.__phase)
+end
+
+function Osc:triangle()
+    return math.abs(
+        ((self.__phase + Math.hpi) % Math.pi2 / Math.pi2 * 2 - 1)
+    ) * -2 + 1
+end
+
+function Osc:square()
+    local state = self.__phase < math.pi
+    if state then return 1 else return -1 end
+end
+
+function Osc:saw()
+    return (self.__phase + Math.hpi) % Math.pi2 / Math.pi2 * 2 - 1
 end
