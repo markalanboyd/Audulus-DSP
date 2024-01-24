af4548a5b6f20c93bfac804c51a206afaf4939eb
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-24 22:55:20 +0100

    Update phasor.lua
    - Add sync

--- src/phasor.lua
+++ src/phasor.lua
@@ -4,25 +4,23 @@ Phasor.__index = Phasor
 
 function Phasor.new()
     local self = setmetatable({}, Phasor)
-
     self.phase = 0
-
     return self
 end
 
-function Phasor:audio(hz)
+function Phasor:audio(hz, sync)
     local phase_increment = hz / sampleRate * Math.pi2
     self.phase = self.phase + phase_increment
-    if self.phase >= Math.pi2 then
+    if self.phase >= Math.pi2 or sync > 0 then
         self.phase = 0
     end
     return self.phase
 end
 
-function Phasor:control(frames, hz)
+function Phasor:control(frames, hz, sync)
     local phase_increment = hz / (sampleRate / frames) * Math.pi2
     self.phase = self.phase + phase_increment
-    if self.phase >= Math.pi2 then
+    if self.phase >= Math.pi2 or sync > 0 then
         self.phase = 0
     end
     return self.phase
