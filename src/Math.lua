bf1c22671276489846d11d225dbbf3ce3e5fb79d
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-25 06:23:09 +0100

    Update Math.lua
    - Precalculate inverse sample rate
    - Add unsafe Math.tanh that is faster than built-in but good for -1 to 1 use cases

--- src/Math.lua
+++ src/Math.lua
@@ -1,6 +1,13 @@
 Math = {}
 M = Math
 
-Math.pi2 = math.pi * 2
-Math.hpi = math.pi * 0.5
-Math.qpi = math.pi * 0.25
+Math.two_pi = math.pi * 2
+Math.half_pi = math.pi * 0.5
+Math.quarter_pi = math.pi * 0.25
+
+Math.inv_sr = 1 / sampleRate
+
+function Math.tanh(x)
+    local e2x = math.exp(2 * x)
+    return (e2x - 1) / (e2x + 1)
+end
