8c37664c00b6306b5d26b509bc601ef7a721fb26
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-26 14:35:20 +0100

    Update Math.lua
    - Add inverse precalculations for fast division/multiplication
    - Add fast fract function for saw wave

--- src/Math.lua
+++ src/Math.lua
@@ -2,6 +2,8 @@ Math = {}
 M = Math
 
 Math.two_pi = math.pi * 2
+Math.inv_two_pi = 1 / Math.two_pi
+Math.inv_pi = 1 / math.pi
 Math.half_pi = math.pi * 0.5
 Math.quarter_pi = math.pi * 0.25
 
@@ -10,4 +12,8 @@ Math.inv_sr = 1 / sampleRate
 function Math.tanh(x)
     local e2x = math.exp(2 * x)
     return (e2x - 1) / (e2x + 1)
+end
+
+function Math.fract(x)
+    return x - math.floor(x)
 end
