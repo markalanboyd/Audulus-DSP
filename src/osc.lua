79b1637d4407f0a5daa652474e239cf60400e0bc
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-26 14:36:22 +0100

    Update osc.lua
    - Almost finished with basic waveforms
    - Still need to fix the saw wave

--- src/osc.lua
+++ src/osc.lua
@@ -15,44 +15,70 @@ function Osc.new()
 end
 
 function Osc:update()
-    self.__phase = self.__phasor:audio(
-        self.hz,
-        self.__gate:to_pulse(self.sync)
-    ) + self.phase_offset * Math.two_pi
+    self.__phase = (
+        self.__phasor:audio(
+            self.hz,
+            self.__gate:to_pulse(self.sync)
+        ) + self.phase_offset * Math.two_pi
+    ) % Math.two_pi
 end
 
 function Osc:sine()
-    local pure_sine = math.sin(self.__phase)
-    return (1 - self.shape) * pure_sine +
+    local raw_sine = math.sin(self.__phase)
+    return (1 - self.shape) * raw_sine +
         self.shape * math.sin(
-            pure_sine * Math.half_pi * (self.shape * self.shape * 19 + 1)
+            raw_sine * Math.half_pi * (self.shape * self.shape * 19 + 1)
         )
 end
 
-function Osc:pure_sine()
+function Osc:raw_sine()
     return math.sin(self.__phase)
 end
 
+-- TODO Reduce operations
+
 function Osc:triangle()
-    local pure_triangle = math.abs(
-        ((self.__phase + Math.half_pi) % Math.two_pi / Math.two_pi * 2 - 1)
+    local raw_triangle = math.abs(
+        ((self.__phase + Math.half_pi) % Math.two_pi * Math.inv_two_pi * 2 - 1)
     ) * -2 + 1
     local factor = self.shape * 4 + 1
     local rescale = math.max(((1 - self.shape) * 1.3), 1)
-    return Math.tanh(pure_triangle * factor) * rescale
+    return Math.tanh(raw_triangle * factor) * rescale
 end
 
-function Osc:pure_triangle()
+function Osc:raw_triangle()
     return math.abs(
-        (self.__phase + Math.half_pi / Math.two_pi * 2 - 1)
+        (self.__phase + Math.half_pi * Math.inv_two_pi * 2 - 1)
     ) * -2 + 1
 end
 
 function Osc:square()
-    local state = self.__phase < math.pi
-    if state then return 1 else return -1 end
+    if self.__phase < self.shape * math.pi + math.pi then
+        return 1
+    else
+        return -1
+    end
 end
 
+function Osc:raw_square()
+    if self.__phase < math.pi then return 1 else return -1 end
+end
+
+-- TODO Ensure -1 to 1 range for saw
+
 function Osc:saw()
-    return (self.__phase + Math.half_pi) % Math.two_pi / Math.two_pi * 2 - 1
+    local saw_shifted = Math.fract(
+        (self.__phase * Math.inv_two_pi) + self.shape * 0.5
+    ) * Math.two_pi * Math.inv_pi - 1
+    local saw = self.__phase * Math.inv_pi - 1
+    local sum = (saw + saw_shifted) * 0.5
+
+    local f = 0.3
+    local r_factor = 1 - (f - math.abs(self.shape * 2 - 1) * f) ^ 2
+
+    return sum * (self.shape + 1) * r_factor, r_factor
+end
+
+function Osc:raw_saw()
+    return self.__phase * Math.inv_two_pi * 2 - 1
 end
